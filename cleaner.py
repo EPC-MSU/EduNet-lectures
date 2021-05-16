0dccf2786ac24cac8b3c22acb2b430065d57677f
Andrey <a.p.marakulin@gmail.com>
2021-05-16 15:11:08 +0300

    Add argparse

--- cleaner.py
+++ cleaner.py
@@ -1,19 +1,8 @@
-import json
 import os
 import re
+import json
 import base64
-
-
-def is_markdown(cell):
-    return cell['cell_type'] == 'markdown'
-
-
-def is_code(cell):
-    return cell['cell_type'] == 'code'
-
-
-def is_raw(cell):
-    return cell['cell_type'] == 'raw'
+import argparse
 
 
 def save_file(filepath, filename, data):
@@ -34,22 +23,21 @@ def save_attachments(lect_path, attachments):
 
 def check_and_fix_source(cell, lect_path, i, total_i):
     cell_text = "".join(cell["source"]).replace("\n", "")
-    if "base64" in cell_text:
-        print(f"\t[WARNING][Cell:{i+1}/{total_i}]: Binary data found.")
+    if "base64" in cell_text and args.warnings:
+        print(f"\t[WARNING][Cell:{i + 1}/{total_i}]: Binary data found.")
         print("\t\t", cell_text[:150].rstrip())
         ctr["warnings"] += 1
 
     if "attachments" in cell:
-        print(f"\t[WARNING][Cell:{i+1}/{total_i}]: Markdown attachments found.")
+        print(f"\t[WARNING][Cell:{i + 1}/{total_i}]: Markdown attachments found.")
         cell_text = "".join(cell["source"]).replace("\n", "")
         print("\t\t", cell_text[:100].rstrip())
         save_attachments(lect_path, cell["attachments"])
         del cell["attachments"]
-
         ctr["attachments"] += 1
     else:
-        if re.match(r'^.*!\[.*\]\(.*\).*$', cell_text) is not None:
-            print(f"\t[WARNING][Cell:{i+1}/{total_i}]: Probably local link found.")
+        if re.match(r'^.*!\[.*\]\(.*\).*$', cell_text) is not None and args.warnings:
+            print(f"\t[WARNING][Cell:{i + 1}/{total_i}]: Probably local link found.")
             print("\t\t", cell_text[:150].rstrip())
             ctr["warnings"] += 1
     return cell
@@ -78,10 +66,6 @@ def fix_markdown_cell(cell):
 def process_one_lecture(pathname, overwrite=False):
     lecture_path = os.path.dirname(pathname)
     notebook_name = os.path.basename(pathname)
-    if overwrite:
-        clear_notebook_patch = pathname
-    else:
-        clear_notebook_patch = os.path.join(lecture_path, notebook_name.split(".")[0] + "_clear.ipynb")
 
     with open(pathname, "r") as inp:
         js = json.load(inp)
@@ -92,23 +76,29 @@ def process_one_lecture(pathname, overwrite=False):
 
     for i, cell in enumerate(js['cells']):
         cell = check_and_fix_source(cell, lecture_path, i, total_i)
-        if is_markdown(cell):
-            newcell = fix_markdown_cell(cell)
-        elif is_code(cell):
-            newcell = fix_code_cell(cell)
-        elif is_raw(cell):
-            newcell = cell
-            print(f"Raw cell: {i+1}/{total_i}. Please fix it.")
+        if cell['cell_type'] == 'markdown':
+            new_cell = fix_markdown_cell(cell)
+        elif cell['cell_type'] == 'code':
+            new_cell = fix_code_cell(cell)
+        elif cell['cell_type'] == 'raw':
+            new_cell = cell
+            print(f"Raw cell: {i + 1}/{total_i}. Please fix it.")
         else:
             raise ValueError(f"Notebook broken. Unknown cell type: {cell['cell_type']}")
 
-        new_js["cells"].append(newcell)
+        new_js["cells"].append(new_cell)
 
-    old_global_meta = {key: value for key, value in js.items() if key != "cells"}
-    new_js.update(old_global_meta)
-    with open(clear_notebook_patch, "w", encoding='utf8') as out:
-        json.dump(new_js, out, indent=1, ensure_ascii=False)
-        out.write('\n')
+    if ctr.is_changed():
+        save_patch = pathname
+        if not overwrite:
+            backup_patch = os.path.join(lecture_path, notebook_name.split(".")[-2] + "_backup.ipynb")
+            os.replace(pathname, backup_patch)
+
+        old_global_meta = {key: value for key, value in js.items() if key != "cells"}
+        new_js.update(old_global_meta)
+        with open(save_patch, "w", encoding='utf8') as out:
+            json.dump(new_js, out, indent=1, ensure_ascii=False)
+            out.write('\n')
 
 
 class Counter(dict):
@@ -133,33 +123,47 @@ class Counter(dict):
     def reset(self):
         self.__init__()
 
+    def is_changed(self):
+        if sum(list(self.values())) - self['warnings'] == 0:
+            return False
+        else:
+            return True
 
-MODE = "all_ipynb"
-### If mode "one_ipynb", specify:
-lecture_pathname = "out//L13_GAN_cGAN//L13_GAN_cGAN.ipynb"
 
-### If MODE == "all_ipynb", specify:
-root = "out"
-
-OVERWRITE = True
-# If OVERWRITE == False, script doesn't change original ipynb and save copy with suffix '_clear.ipynb' instead.
-
-if __name__ == "__main__":
-    ctr = Counter()
-    if MODE == "one_ipynb":
+def main():
+    if args.filepath is not None:
+        lecture_pathname = args.filepath
         print(lecture_pathname)
         ctr.reset()
-        process_one_lecture(lecture_pathname, overwrite=OVERWRITE)
+        process_one_lecture(lecture_pathname, overwrite=args.overwrite)
         ctr.summary()
-    if MODE == "all_ipynb":
-        for path, subdirs, files in os.walk(root):
+    else:
+        for path, subdirs, files in os.walk(args.root):
             for name in files:
                 if name.endswith('.ipynb') and \
                         ".ipynb_checkpoints" not in path and \
-                        not name.endswith('_clear.ipynb'):
+                        not name.endswith('_backup.ipynb'):
                     lecture_pathname = os.path.join(path, name)
 
                     print(lecture_pathname)
                     ctr.reset()
-                    process_one_lecture(lecture_pathname, overwrite=OVERWRITE)
+                    process_one_lecture(lecture_pathname, overwrite=args.overwrite)
                     ctr.summary()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script for cleaning notebooks.')
+    parser.add_argument('--disable-backup', dest='overwrite', action='store_true',
+                        help="If provided, overwrites target notebook, else create a ipynb backup.")
+    parser.add_argument('--disable-warnings', dest='warnings', action='store_false',
+                        help="Script disable warnings like 'Probably local link found.'")
+    parser.add_argument('--filepath', default=None,
+                        help='Notebook filepath, if not provided, script processed all files in root.')
+    parser.add_argument('--root', default="out",
+                        help="""(default:"out") Processed all file in root folder and all subfolders.""")
+    parser.set_defaults(overwrite=False)
+    parser.set_defaults(warnings=True)
+
+    args = parser.parse_args()
+    ctr = Counter()
+    main()
