b107da5b4fb1e760d5fa30a5fbf9ef4859102b3c
brinza <bezrukoff888@gmail.com>
2022-01-14 16:29:52 +0300

    Added argparser

--- autotc.py
+++ autotc.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import logging
+import argparse
 
 
 logger = logging.getLogger(__name__)
@@ -12,18 +13,16 @@ fmt = logging.Formatter("[%(levelname)s] [%(asctime)s] %(name)s: %(message)s")
 ch.setFormatter(fmt)
 logger.addHandler(ch)
 
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-out_dir = os.path.join(base_dir, "out")
-cur_file = os.path.join(out_dir, "Curriculum.md")
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", help="Directory with notebooks to process", default="out")
+parser.add_argument("--output", help="Output filename", default="temp_Curriculum.md")
 
 lpattern = re.compile(r"L\d+(_\w*)?\.ipynb")  # filename pattern
 tpattern = re.compile(r".*<.*>(.*)<\/.*>.*")  # title pattern
 hpattern = re.compile(r"#{1,2}\s*([^#\n<>]+)")  # header pattern
 
 
-def analyze_lecture(fname):
-    path = os.path.join(out_dir, fname)
+def analyze_lecture(path):
     with open(path, encoding="utf-8") as f:
         lecture = json.load(f)
     cells = lecture.get("cells")
@@ -33,8 +32,8 @@ def analyze_lecture(fname):
     title_src = "".join(cells[0]["source"])
     title = tpattern.match(title_src)
     if not title:
-        logger.warning(f"Lecture's title not found, using name '{fname}'")
-        title = fname
+        title = os.path.basename(path)
+        logger.warning(f"Lecture's title not found, using name '{title}'")
     else:
         title = title.group(1).strip()
     headers = []
@@ -58,25 +57,20 @@ def generate_md(lectures, path):
         print(". ".join(headers) + "\n", file=f)
         i += 1
     f.close()
+    logger.info(f"File created: {path}")
 
 
 def main():
+    args = parser.parse_args()
+    out_dir = os.path.abspath(args.dir)
     lectures = {}
     for fname in sorted(os.listdir(out_dir)):
         if not lpattern.fullmatch(fname):
             continue
-        title, headers = analyze_lecture(fname)
+        title, headers = analyze_lecture(os.path.join(out_dir, fname))
         lectures[title] = headers
-
     logger.info(f"Total analyzed: {len(lectures)}")
-    
-    if os.path.isfile(cur_file):
-        logger.info("Curriculum.md exists, writing in Curriculum_Temp.md")
-        path = os.path.join(out_dir, "Curriculum_Temp.md")
-    else:
-        path = cur_file
-    
-    generate_md(lectures, path)
+    generate_md(lectures, os.path.join(out_dir, args.output))
 
 
 if __name__ == "__main__":
